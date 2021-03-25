48f0e266661ac0027e9a0eb7cf0c52d26e48a10a
sadiq <you@example.com>
2021-03-25 16:11:49 +0500

    People listing api is completed.

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PeopleResource;
 use App\Models\People;
+use \Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PeopleController extends Controller
 {
@@ -11,4 +14,49 @@ class PeopleController extends Controller
 	{
 		return view('people');
 	}
+
+    /**
+     * @return AnonymousResourceCollection
+     *
+     * First get people list from database and if not found in database,
+     * Then fetch them from Airtable Api and cache them for subsequent requests.
+     */
+    public function list(): AnonymousResourceCollection
+    {
+        $people = People::list();
+
+        if ($people->isEmpty())
+            $people = $this->fetchAndCachePeople();
+
+        return PeopleResource::collection($people);
+	}//..... end of list() .....//
+
+    /**
+     * @return Collection|\Illuminate\Support\Collection
+     *
+     * Fetch records from Airtable and then insert them into Database.
+     */
+    private function fetchAndCachePeople(): Collection | \Illuminate\Support\Collection
+    {
+        $airTablePeople = (new \App\Services\People())->get();
+
+        if ($airTablePeople and isset($airTablePeople['records'])) {
+            $people = [];
+
+            foreach ($airTablePeople['records'] as $record)
+                $people[] = [
+                    'airtable_id'   => $record['id'],
+                    'name'          => $record['fields']['Name'] ?? '',
+                    'email'         => $record['fields']['Email'] ?? '',
+                    'photo'         => json_encode($record['fields']['Photo'] ?? []),
+                    'status'        => 1
+                ];
+
+            People::bulkInsert($people);
+
+            return People::list();
+        }//..... end if() ....//
+
+        return collect([]);
+	}//..... end of fetchAndCachePeople() .....//
 }
--- app/Models/People.php
+++ app/Models/People.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
 
 class People extends Model
 {
@@ -11,4 +13,36 @@ class People extends Model
 
     protected $table = 'people';
     protected $guarded = ['id'];
+
+    /**
+     * @return Collection
+     * Fetch records from database.
+     */
+    public static function list(): Collection
+    {
+        return People::all();
+    }//..... end of list() .....//
+
+    /**
+     * @param $records
+     * Bulk insert.
+     */
+    public static function bulkInsert($records): void
+    {
+        try {
+            People::insert($records);
+        } catch (\Exception $exception) {
+            Log::error('Could not insert records into database: '. $exception->getMessage());
+        }//..... end of try-catch() .....//
+    }//...... end of bulkInsert() .....//
+
+    /**
+     * @param $photo
+     * @return array|mixed
+     * Photo field accessor.
+     */
+    public function getPhotoAttribute($photo)
+    {
+        return $photo ? json_decode($photo, true) : [];
+    }//..... end of getPhotoAttribute() .....//
 }
